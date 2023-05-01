d33695409cbc087745994efd570a1b28f1b804ac
nkong008 <nkong2020@gmail.com>
2023-05-01 12:22:29 -0500

    added full cursor animation

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    // let mouseCursor = $(".cursor");
+    // let navLinks = $(".nav-links li");
+    let mouseCursor = document.querySelector(".cursor");
+    let navLinks = document.querySelectorAll(".nav-links li");
+    console.log(navLinks);
+    console.log(mouseCursor);
+
+    window.addEventListener("mousemove", cursorMovement);
+    // e is event
+    function cursorMovement(e) {
+        mouseCursor.style.top = e.pageY + "px"
+        mouseCursor.style.left = e.pageX + "px"
+    }
+    navLinks.forEach((link) => {
+        link.addEventListener("mouseleave", ()=> {
+            mouseCursor.classList.remove("link-grow");
+            link.classList.remove("hovered-link");
+        });
+
+        link.addEventListener("mouseover", ()=> {
+            mouseCursor.classList.add("link-grow");
+            link.classList.add("hovered-link");
+        });
+
+    });
+        
+    
+}); //document ends here 
